5d5bb4c56e14b40703d9dd7228a51db2f81acd18
DESKTOP-GQ4UC8I\bitcamp <soobin9257@naver.com>
2021-01-15 18:07:52 +0900

    # WARNING: head commit changed in the meantime
    ㅜㅜㅜㅜ

--- src/main/java/spring/member/controller/MemberController.java
+++ src/main/java/spring/member/controller/MemberController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import spring.dto.MemberDto;
 import spring.member.service.MemberServiceInter;
@@ -15,18 +17,21 @@ public class MemberController {
 	@Autowired
 	private MemberServiceInter service;
 	
+	//로그인창
 	@GetMapping("/member/login")
 	public String goLogin()
 	{
 		return "/member/login";
 	}
 	
+	//가입선택
 	@GetMapping("/member/signup")
 	public String goSignUp()
 	{
 		return "/member/signup";
 	}
 	
+	//일반가입or의사가입창
 	@GetMapping("/member/msignup")
 	public String goMSignUp()
 	{
@@ -39,6 +44,8 @@ public class MemberController {
 		return "/member/dsignup";
 	}
 	
+	
+	//insert
 	@PostMapping("member/savemember")
 	public String insertMember(@ModelAttribute MemberDto dto)
 	{
@@ -46,5 +53,6 @@ public class MemberController {
 		service.insertMember(dto);
 		return "redirect:login";
 	}
+	//////////////////////////////////////
 
 }
