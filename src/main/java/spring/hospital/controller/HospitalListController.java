6983a3cd1507f35fa396867a5ccae2783a986e7c
jjeong <jjeong@localhost>
2021-01-18 20:47:21 +0900

    card update

--- src/main/java/spring/hospital/controller/HospitalListController.java
+++ src/main/java/spring/hospital/controller/HospitalListController.java
@@ -5,19 +5,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HospitalListController {
-	@GetMapping("/hospitalmain")
-   public String goHospital() {
-      return "/hospital/hospitalmain";
-   }
-	
-   @GetMapping("/hospitallist")
-   public String goHlist() {
-      return "/hospital/hospitallist";
-   }
-   
-   @GetMapping("/hospitaldetail")
-   public String goHdetail() {
-      return "/hospital/hospitaldetail";
-   }
+//	@GetMapping("/hospitalmain")
+//   public String goHospital() {
+//      return "/hospital/hospitalmain";
+//   }
+//	
+//   @GetMapping("/hospitallist")
+//   public String goHlist() {
+//      return "/hospital/hospitallist";
+//   }
+//   
+//   @GetMapping("/hospitaldetail")
+//   public String goHdetail() {
+//      return "/hospital/hospitaldetail";
+//   }
 
 }
