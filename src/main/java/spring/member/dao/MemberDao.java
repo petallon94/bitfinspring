ff98be77fb0112864e046fcbcf88ca2c100e2852
hansaebit <hansaebit@gmail.com>
2021-01-21 15:51:42 +0900

    memberdao 오류 수정

--- src/main/java/spring/member/dao/MemberDao.java
+++ src/main/java/spring/member/dao/MemberDao.java
@@ -67,7 +67,6 @@ public class MemberDao extends SqlSessionDaoSupport implements MemberDaoInter {
 		params.put("mpw", mpw);
 		System.out.println(mid+","+mpw);
 		System.out.println(params);
-		System.out.println(getSqlSession().selectOne("loginOfMember",params));
 	
 		return getSqlSession().selectOne("loginOfMember",params);
 	}
