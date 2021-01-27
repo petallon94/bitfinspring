1aab58ad40c2e03e7c11918238c138de40f4e958
jjeong <minjungmel@gmail.com>
2021-01-27 20:54:31 +0900

    chat update

--- src/main/java/spring/chat/dao/ChatDaoInter.java
+++ src/main/java/spring/chat/dao/ChatDaoInter.java
@@ -1,12 +1,6 @@
 package spring.chat.dao;
 
-import java.util.ArrayList;
-import java.util.Map;
 
 public interface ChatDaoInter {
 
-	public ArrayList<ChatDto> getChatList(String chtime);
-
-	public void add(Map<String, Object> param);
-	
 }
--- src/main/java/spring/chat/dao/ChatService.java
+++
@@ -1,9 +0,0 @@
-package spring.chat.dao;
-
-import java.util.Map;
-
-public interface ChatService {
-
-	public Map<String, Object> doChatAdd(Map<String, Object> param);
-
-}
