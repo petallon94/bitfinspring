9fe914d960febc2ed6833dd9fc9fe36c9c4f1833
HANSAEBIT <hansaebit@gmail.com>
2021-01-27 20:57:39 +0900

    지도 그래프///?

--- src/main/webapp/resources/js/chart.js
+++ src/main/webapp/resources/js/chart.js
@@ -3,13 +3,22 @@ $(function(){
 	inflectionpeople=inflectionpeople.replace("\[","");
 	inflectionpeople=inflectionpeople.replace("\]","");
 	inflectionpeople=inflectionpeople.split(",");
-
+	
 	var inflectiondate = $('input[name=inflectiondate]').val();
 	inflectiondate=inflectiondate.replace("\[","");
 	inflectiondate=inflectiondate.replace("\]","");
 	inflectiondate=inflectiondate.split(",");
-
 	
+	var inflectgubunResult = $('input[name=inflectgubunResult]').val();
+	inflectgubunResult=inflectgubunResult.replace("\[","");
+	inflectgubunResult=inflectgubunResult.replace("\]","");
+	inflectgubunResult=inflectgubunResult.split(",");
+	
+	var inflectmapResult = $('input[name=inflectmapResult]').val();
+	inflectmapResult=inflectmapResult.replace("\[","");
+	inflectmapResult=inflectmapResult.replace("\]","");
+	inflectmapResult=inflectmapResult.split(",");
+
 	var mychart = $('#line-chart');
 	var myLineChart =  new Chart(mychart, {
 		type:'line',
@@ -18,19 +27,11 @@ $(function(){
 			datasets:[
 				{
 					data:inflectionpeople,
-					backgroundColor:'rgba(255,0,0,0.5)',
-					borderColor:'rgb(200,0,0)',
-					borderWidth:1,
-					/*lineTension:0,*/
-					/*borderDash:[10,10],*/
-					fill:false,
+					backgroundColor:'rgba(0,0,255,0.3)',
+					borderColor:'rgba(64,71,96,1.0)',
 					pointRadius:4,
 					pointHoverRadius:8,
-					pointBorderColor:'black',
-					pointBackgroundColor:'yellow',
-					pointHoverBorderColor:'yellow',
-					pointHoverBackgroundColor:'black'
-					
+					pointBackgroundColor:'rgba(19,58,168,1.0)'
 				}
 			]
 		},
@@ -39,8 +40,51 @@ $(function(){
 		}
 	});	
 	
-});
+	var data = {
+	    datasets: [{
+	        data: inflectmapResult,
+	        backgroundColor: [
+	            "#eccc68",
+	            "#ff7f50",
+	            "#ff6b81",
+	            "#a4b0be",
+	            "#ffa502",
+				"#ff6348",
+				"#ff4757",
+				"#7bed9f",
+				"#70a1ff",
+				"#5352ed",
+				"#2ed573",
+				"#1e90ff",
+				"#3742fa",
+				"",
+				"",
+				"",
+				"",
+				"",
+	        ],
+	        label: 'My dataset' // for legend
+	    }],
+	    labels: inflectgubunResult
+	};
+	var ctx = $("#polar-chart");
+	new Chart(ctx, {
+	    data: data,
+	    type: 'polarArea'
+	});
 	
+});
+
+
+/*borderColor:'rgba(64,71,96,1.0)',
+borderWidth:3,
+fill:false,
+pointRadius:4,
+pointHoverRadius:8,
+pointBorderColor:'rgba(19,58,168,1.0)',
+pointBackgroundColor:'rgba(19,58,168,1.0)',
+pointHoverBorderColor:'rgba(223,227,239,1.0)',
+pointHoverBackgroundColor:'rgba(223,227,239,1.0)',*/
 
 	
 
